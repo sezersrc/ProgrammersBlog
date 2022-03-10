168b24c44ba0b64017d2fa591a5c9ecfd735500c
Sezer Sürücü <sezer@sezersurucu.com>
2022-03-10 16:02:33 +0300

    Add project files.

---
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgrammersBlog.Shared.Entities.Abstract;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    public class Category:EntityBase,IEntity
+    {
+       /// public override string CreatedByName { get; set; } = "Admin"; Override Örneği;
+       public string Name { get; set; }
+
+       public string Description { get; set; }
+       public ICollection<Article> Articles { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        // base değerlerin diğer sınıflarda değiştirebilmek için Abstract  ekliyoruz . Override etmeye yarıyor . 
+
+        public  virtual int Id { get; set; }
+        public virtual  DateTime CreatedDate { get; set; } = DateTime.Now; // override CreatedDate {get;set;} = new Datetime(2020/01/01); Örnek
+        public virtual DateTime  ModifiedDate { get; set; }=DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; } = "Admin"; // Default Değer Atama
+        public virtual string ModifiedByName { get; set; } = "Admin";
+        public virtual string Note { get; set; }
+    }
+}
