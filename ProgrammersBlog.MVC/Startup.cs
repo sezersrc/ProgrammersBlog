cc9eab8d19b5b4da2c4f69fd159f524453f2e0e2
Sezer Sürücü <sezer@sezersurucu.com>
2022-03-13 13:44:39 +0300

    Nuget && Startup Config

--- ProgrammersBlog.MVC/Startup.cs
+++ ProgrammersBlog.MVC/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProgrammersBlog.Services.Extensions;
 
 namespace ProgrammersBlog.MVC
 {
@@ -23,7 +24,11 @@ namespace ProgrammersBlog.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation(); // MVC olduðunu belirteci && Razor Runtime Compalition 
+            services.AddAutoMapper(typeof(Startup)); // Derlenme esnasýnda AutoMapper'ý derliyor . Mapping sýnýflarýný çaðýrýyor.
+
+            // Service katmanýndan servis yüklenmek . Data'ya direk ulaþmýyor Service >Data aktarýp oradan çekiyor.
+            services.LoadMyServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +37,7 @@ namespace ProgrammersBlog.MVC
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();// Sitede bulunmayan bir view'A gidildiðinde 404 kodu döner.
             }
             else
             {
@@ -40,8 +46,10 @@ namespace ProgrammersBlog.MVC
                 app.UseHsts();
             }
 
+          
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles();  // Statik dosya java , html gibi dosyalarý kullandýrýr.
 
             app.UseRouting();
 
@@ -49,7 +57,13 @@ namespace ProgrammersBlog.MVC
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                // Admin Raouting => Tek Areaiçin 
+                endpoints.MapAreaControllerRoute(
+                    name: "Admin",
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=home}/{action=Index}/{id?}"
+                );
+                endpoints.MapDefaultControllerRoute();  // Varsayýlan olarak Hom Controller'A götürür.
             });
         }
     }
---
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Data.Concrete;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Services.Concrete;
+
+namespace ProgrammersBlog.Services.Extensions
+{
+    public  static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            // Data KAtmanındaki  yapıları MVC KAtmanına aktarmaya yarayan servis .Startup.CS'den çağırdık.
+            serviceCollection.AddDbContext<ProgrammersBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+            return serviceCollection;
+        }
+    }
+}
