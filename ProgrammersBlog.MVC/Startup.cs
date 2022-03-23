a254a9646ffe667bf21f9ba43f3ef8069749e9e5
Sezer Sürücü <sezer@sezersurucu.com>
2022-03-23 10:32:23 +0300

    identity cookie config

--- ProgrammersBlog.MVC/Startup.cs
+++ ProgrammersBlog.MVC/Startup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using ProgrammersBlog.MVC.Controllers;
 using ProgrammersBlog.Services.AutoMapper.Profiles;
 using ProgrammersBlog.Services.Extensions;
@@ -32,10 +33,27 @@ namespace ProgrammersBlog.MVC
                 opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); // Enum deðerleri döndürür (JsonNamingPolicy:CamelCase olursa string girebilirsin)
                 opt.JsonSerializerOptions.ReferenceHandler=ReferenceHandler.Preserve;// iç içe olan (nested ) json'A çevirir.
             }); // MVC olduðunu belirteci && Razor Runtime Compalition . // optJson'A dönüþtürmek için.
+            services.AddSession();
             services.AddAutoMapper(typeof(CategoryProfile),typeof(ArticleProfile)); // *Profil sýnýfý olmak zorunda Derlenme esnasýnda AutoMapper'ý derliyor . Mapping sýnýflarýný çaðýrýyor.
 
             // Service katmanýndan servis yüklenmek . Data'ya direk ulaþmýyor Service >Data aktarýp oradan çekiyor.
             services.LoadMyServices();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = new PathString("/Admin/User/Login");
+                options.LogoutPath = new PathString("/Admin/User/Logout");
+                options.Cookie = new CookieBuilder
+                {
+                    Name = "ProgrammersBlog",
+                    HttpOnly = true,
+                    SameSite = SameSiteMode.Strict,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest, // Güvenlik açýðý always'a çekersin SSL'e yönlenir.
+
+                };
+                options.SlidingExpiration = true;
+                options.ExpireTimeSpan = System.TimeSpan.FromDays(7);
+                options.AccessDeniedPath = new PathString("/Admin/User/AccessDenied");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,16 +71,16 @@ namespace ProgrammersBlog.MVC
                 app.UseHsts();
             }
 
-          
 
-            app.UseHttpsRedirection();
+            app.UseSession();
+            app.UseHttpsRedirection();  // Projede yok ;
             app.UseStaticFiles();  // Statik dosya java , html gibi dosyalarý kullandýrýr.
 
             app.UseRouting();
-            app.UseCors(); // Startup hatasý çözümü.
+            app.UseCors(); // Startup hatasý çözümü. // projede yok
 
-            app.UseAuthentication(); // Startup hatasý verdi çözümü.
-            app.UseAuthorization();
+            app.UseAuthentication(); // Identity => Authentication 
+            app.UseAuthorization();  // Identity => Authorization  
 
 
             app.UseEndpoints(endpoints =>
