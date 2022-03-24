3c642497286eb04f643464ba523987392195a369
Sezer Sürücü <sezer@sezersurucu.com>
2022-03-24 17:28:10 +0300

    user login dto

---
+++ ProgrammersBlog.Entities/Dtos/UserLoginDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserLoginDto
+    {
+        [DisplayName("Eposta Adresi")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir !..")]
+        [MaxLength(100, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır. ")]
+        [MinLength(10, ErrorMessage = "{0} {1} karakterden küçük olamaz. ")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir !..")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır. ")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden küçük olamaz. ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Telefon Numarası")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir !..")]
+        [MaxLength(13, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır. ")]
+        [MinLength(13, ErrorMessage = "{0} {1} karakterden küçük olamaz. ")]
+        [DataType(DataType.PhoneNumber)]
+
+        [DisplayName("Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
