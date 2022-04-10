dca037f610b7d3e453e192f66c13206360bf3e3a
Sezer Sürücü <sezer@sezersurucu.com>
2022-04-10 19:01:30 +0300

    Roles Part 1

---
+++ ProgrammersBlog.MVC/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+
+namespace ProgrammersBlog.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<Role> _roleManager;
+
+        public RoleController(RoleManager<Role> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "SuperAdmin,Role.Read")]
+        
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(new RoleListDto
+            {
+                Roles = roles
+            });
+        }
+
+        [Authorize(Roles = "SuperAdmin,Role.Read")]
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            var rolesDto = JsonSerializer.Serialize(new RoleListDto { Roles = roles });
+            return Json(rolesDto);
+            
+        }
+    }
+}
